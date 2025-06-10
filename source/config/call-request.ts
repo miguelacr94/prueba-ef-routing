913a3d856d177078097eb4824eb01ddeb1ae9aec
Miguel C <miguelacr94@gmail.com>
2025-06-10 13:13:45 -0500

    creacion de call-request para consumo de api e instalacion de axios

---
+++ source/config/call-request.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { API_URL } from "../utils/constanst";
+
+export const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
